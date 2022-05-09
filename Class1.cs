8edcc17e0e41e00513302a5b1e885ca9350a076d
Kaleeshkumar R <kaleeshkumar.r@gmail.com>
2022-05-09 19:56:40 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INHERITANCE
+{
+    internal class Student
+    {
+        public int tot, sub;
+        public float avg;
+        public string grade;
+    }
+
+    class School : Student
+    {
+        public string gradecal()
+        {
+            avg = tot/sub;
+            if (avg <= 90&& avg<=100)
+            {
+                grade ="A";
+            }
+            else
+            {
+                grade ="B";
+            }
+            return grade;
+
+        }
+    }
+        class College : Student
+        {
+            public string gradecal()
+            {
+                avg = tot/sub;
+                if (avg <= 90&& avg<=100)
+                {
+                    grade="1";
+                }
+                else
+                {
+                    grade="2";
+                }
+                return grade;
+            }
+        }
+    }
+
+        
